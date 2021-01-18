dab67fc106e38327b20877a631094488d32bc535
JeffreyCA <jeffreyca16@gmail.com>
2021-01-18 09:47:41 -0500

    Update files

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xumx", # Replace with your own username
-    version="0.0.1",
-    author="Sony",
+    name="jeffreyca-xumx",
+    version="0.1.0",
+    author="JeffreyCA",
     author_email="jeffreyca16@gmail.com",
-    description="X-UMX",
+    description="Unofficial NNabla implementation of CrossNet-Open-Unmix (X-UMX), originally created by Sony Research AI.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/JeffreyCA/ai-research-code",
+    url="https://github.com/JeffreyCA/xumx",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
@@ -28,6 +28,7 @@ setuptools.setup(
         'norbert>=0.2.1',
         'resampy==0.2.2',
         'nnabla',
+        'pydub'
     ],
     python_requires='>=3.6',
 )
